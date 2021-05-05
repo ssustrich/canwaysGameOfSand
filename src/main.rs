cf26b5d8c9ec66992c1011eb5ef2aacc18965e3e
ssustrich <maxximillian@gmail.com>
2021-05-05 07:04:48 +0200

    sepertaed out l;ibrary data

--- src/main.rs
+++ src/main.rs
@@ -2,26 +2,21 @@
 #![forbid(unsafe_code)]
 
 use log::{debug, error, info, warn};
+
 use pixels::{Error, Pixels, SurfaceTexture};
-use rand::Rng;
+
 use winit::dpi::{LogicalPosition, LogicalSize, PhysicalSize};
 use winit::event::{Event, VirtualKeyCode};
 use winit::event_loop::{ControlFlow, EventLoop};
 use winit_input_helper::WinitInputHelper;
 use env_logger::Env;
 
+extern crate lib;
+use crate::lib::SandGrid;
 
-const SCREEN_WIDTH: u32 = 1;
-const SCREEN_HEIGHT: u32 = 4;
-const INITIAL_FILL: f32 = 0.9;
-const PARTICLETYPES: [&str; 3] = ["NONE", "SAND", "WATER"];
-const GRAVITY: f32 = 9.8;
-enum OBJECT_TYPES {
-    EMPTY,
-    WALL,
-    SAND,
-    WATER,
-}
+const SCREEN_WIDTH: u32 = 3;
+const SCREEN_HEIGHT: u32 = 10;
+
 
 fn main() -> Result<(), Error> {
     env_logger::Builder::from_env(Env::default().default_filter_or("error,conways_gos=warn,conways_gos=info/update")).init();
@@ -34,8 +29,8 @@ fn main() -> Result<(), Error> {
 
     let surface_texture = SurfaceTexture::new(p_width, p_height, &window);
 
-    //let mut frame = ConwayGrid::new_random(SCREEN_WIDTH as usize, SCREEN_HEIGHT as usize);
-    let mut frame = ConwayGrid::new_empty(SCREEN_WIDTH as usize, SCREEN_HEIGHT as usize);
+    //let mut frame = SandGrid::new_random(SCREEN_WIDTH as usize, SCREEN_HEIGHT as usize);
+    let mut frame = SandGrid::new_empty(SCREEN_WIDTH as usize, SCREEN_HEIGHT as usize);
     let mut pixels = Pixels::new(SCREEN_WIDTH, SCREEN_HEIGHT, surface_texture)?;
     let mut paused = false;
 
@@ -79,8 +74,8 @@ fn main() -> Result<(), Error> {
             if input.key_pressed(VirtualKeyCode::Key2) {
                 frame.set_brush_type(2)
             }
-            if input.key_pressed(VirtualKeyCode::R) {
-                frame.randomize();
+            if input.key_pressed(VirtualKeyCode::C) {
+                frame.clear();
             }
             // Handle mouse. This is a bit involved since support some simple
             // line drawing (mostly because it makes nice looking patterns).
@@ -199,383 +194,4 @@ fn create_window(
     )
 }
 
-/// Generate a pseudorandom seed for the game's PRNG.
-fn generate_seed() -> (u64, u64) {
-    use byteorder::{ByteOrder, NativeEndian};
-    use getrandom::getrandom;
 
-    let mut seed = [0_u8; 16];
-
-    getrandom(&mut seed).expect("failed to getrandom");
-
-    (
-        NativeEndian::read_u64(&seed[0..8]),
-        NativeEndian::read_u64(&seed[8..16]),
-    )
-}
-
-#[derive(Clone, Copy, Debug, Default)]
-/// The most basic element in teh game 
-struct Particle {
-    p_type: usize,
-    active: bool,
-    already_updated: bool,
-    velocity: f32,
-}
-
-impl Particle {
-    fn new(p_type: usize, active: bool) -> Self {
-        Self {
-            p_type,
-            active,
-            already_updated: false,
-            velocity: 1.0,
-        }
-    }
-
-    #[must_use]
-    fn next_state(mut self, active: bool) -> Self {
-        self.active = active;
-        self
-    }
-
-    fn set_active(&mut self, active: bool) {
-        *self = self.next_state(active);
-    }
-}
-
-#[derive(Clone, Debug)]
-struct ConwayGrid {
-    particles: Vec<Particle>,
-    width: usize,
-    height: usize,
-    active_type: usize,
-    // Should always be the same size as `cells`. When updating, we read from
-    // `cells` and write to `scratch_cells`, then swap. Otherwise it's not in
-    // use, and `cells` should be updated directly.
-    scratch_cells: Vec<Particle>,
-}
-
-impl ConwayGrid {
-    fn new_empty(width: usize, height: usize) -> Self {
-        assert!(width != 0 && height != 0);
-        let size = width.checked_mul(height).expect("too big");
-        Self {
-            particles: vec![Particle::default(); size],
-            scratch_cells: vec![Particle::default(); size],
-            active_type: 1,
-            width,
-            height,
-        }
-    }
-    fn set_brush_type(&mut self, brush_type: usize){
-        self.active_type = brush_type;
-    }
-
-    fn new_random(width: usize, height: usize) -> Self {
-        let mut result = Self::new_empty(width, height);
-        result.randomize();
-        result
-    }
-    
-
-    fn randomize(&mut self) {
-        let mut rng: randomize::PCG32 = generate_seed().into();
-        for c in self.particles.iter_mut() {
-            let mut rng1 = rand::thread_rng();
-            //println!("Integer: {}", rng1.gen_range(0..PARTICLETYPES.len());
-            *c = Particle::new(rng1.gen_range(0..PARTICLETYPES.len()), true);
-        }
-        // run a few simulation iterations for aesthetics (If we don't, the
-        // noise is ugly)
-        for _ in 0..3 {
-            self.update();
-        }
-    }
-
-    fn update_water(&mut self, idx: usize){
-        log::debug!("{:?}", self.particles[idx]);
-        //check to see if we can move down
-        let mut v: Vec<isize> = self.getEightNeighbors(idx);
-        let mut ui = v[6];
-        let mut ul = v[5];
-        let mut ur = v[7];
-        
-        //we hit the bottom
-        if ui == -1 {
-            self.particles[idx].active = true; 
-            self.particles[idx].already_updated = true;
-        }
-        else if ui > -1 && self.particles[ui as usize].active {
-            if rand::random(){
-                ul ^= ur;
-                ur ^= ul;
-                ul ^= ur;
-            }
-       
-            //check bl  (which may be swapped)
-            if ul > -1 && !self.particles[ui as usize].active {
-                self.particles[idx].already_updated = true;
-                self.particles[idx].active = false;
-                self.particles[ul as usize].active = true;
-                self.particles[ul as usize].p_type = self.particles[idx].p_type;
-                self.particles[idx].p_type = 0;
-            }
-            else if ur > -1 && !self.particles[ur as usize].active {
-                self.particles[idx].already_updated = true;
-                self.particles[idx].active = false;
-                self.particles[ur as usize].active = true;
-                self.particles[ur as usize].p_type = self.particles[idx].p_type;
-                self.particles[idx].p_type = 0;
-
-
-            } else{
-            self.particles[idx].already_updated = true;
-            self.particles[idx].active = true; 
-            }
-        }
-        else {
-            self.particles[idx].already_updated = true;
-            self.particles[idx].active = false;
-
-            self.particles[ui as usize].active = true;
-            self.particles[ui as usize].p_type = self.particles[idx].p_type;
-            self.particles[idx].p_type = 0;
-        }
-
-
-    }
-    fn update_sand(&mut self, idx: usize){ 
-
-        log::debug!("{:?}", self.particles[idx]);
-        //check to see if we can move down
-        let mut v: Vec<isize> = self.getEightNeighbors(idx);
-        let mut bi = v[2];
-        let mut bl = v[3];
-        let mut br = v[1];
-        
-        //we hit the bottom
-        if bi == -1 {
-            self.particles[idx].active = false; 
-            self.particles[idx].already_updated = true;
-        }
-        else /*&& self.particles[bi as usize].active */{
-            // if rand::random(){
-            //     bl ^= br;
-            //     br ^= bl;
-            //     bl ^= br;
-            // }
-       
-            //check bl  (which may be swapped)
-/*            if bl > -1 && !self.particles[bi as usize].active {
-                self.particles[idx].already_updated = true;
-               // self.particles[idx].active = false;
-             //   self.particles[bl as usize].active = true;
-                self.particles[bl as usize].p_type = self.particles[idx].p_type;
-                self.particles[idx].p_type = 0;
-            }
-            else if br > -1 && !self.particles[br as usize].active {
-                self.particles[idx].already_updated = true;
-              //  self.particles[idx].active = false;
-             //   self.particles[br as usize].active = true;
-                self.particles[br as usize].p_type = self.particles[idx].p_type;
-                self.particles[idx].p_type = 0;
-
-
-            } */
-            
-          //  else{
-            self.particles[idx].already_updated = true;
-            if !self.particles[bi as usize].p_type == 0{
-              self.particles[idx].active = self.particles[bi as usize].active;
-            }
-            else {
-                self.particles[idx].already_updated = true;
-                self.particles[idx].active = false;
-    
-                self.particles[bi as usize].active = true;
-                self.particles[bi as usize].p_type = self.particles[idx].p_type;
-                self.particles[idx].p_type = 0;
-            }
-        }
-        // else {
-        //     self.particles[idx].already_updated = true;
-        //     self.particles[idx].active = false;
-
-        //     self.particles[bi as usize].active = true;
-        //     self.particles[bi as usize].p_type = self.particles[idx].p_type;
-        //     self.particles[idx].p_type = 0;
-        // }
-
-       }
-
-    fn update(&mut self) {
-        for idx in (0..self.particles.len()).rev() {
-                //let neibs = self.count_neibs(x, y);
-             //  println!("Checking for alive cell at index {:?}", self.particles[idx]);
-               //self.printCrazy8(v, idx);
-
-               if !self.particles[idx].already_updated && self.particles[idx].active {
-
-                if self.particles[idx].p_type ==1 {
-                    self.update_sand(idx);
-                }
-                else if self.particles[idx].p_type ==2 {
-                    self.update_sand(idx);
-                }
-               }
-        }
-
-        // for y in 0..self.height {
-        //     for x in 0..self.width {
-        //         let idx = x + y * self.width;
-        //         self.particles[idx].already_updated = false;
-        //     }
-        // }
-    }
-
-    fn toggle(&mut self, x: isize, y: isize) -> bool {
-        if let Some(i) = self.grid_idx(x, y) {
-            let was_active = self.particles[i].active;
-            self.particles[i].set_active(!was_active);
-            self.particles[i].already_updated = false;
-            self.particles[i].p_type = self.active_type;
-            !was_active
-        } else {
-            false
-        }
-    }
-
-    fn draw(&self, screen: &mut [u8]) {
-        debug_assert_eq!(screen.len(), 4 * self.particles.len());
-        for (c, pix) in self.particles.iter().zip(screen.chunks_exact_mut(4)) {
-            let mut color = [0, 0x00, 0x00, 0x00];
-
-            if c.p_type ==1{
-                color = [0x96, 0x4b, 0x00, 0xff];    
-            }
-            else if c.p_type == 2{
-                color = [0, 0xff, 0xff, 0xff];
-            }
-
-            pix.copy_from_slice(&color);
-           /*
-            let color = if c.active {
-                [0, 0xff, 0xff, 0xff]
-            } else {
-                [0, 0, 0, 0xff]
-            };
-            pix.copy_from_slice(&color); */
-        }
-    }
-
-    fn set_line(&mut self, x0: isize, y0: isize, x1: isize, y1: isize, active: bool) {
-        // probably should do sutherland-hodgeman if this were more serious.
-        // instead just clamp the start pos, and draw until moving towards the
-        // end pos takes us out of bounds.
-        let x0 = x0.max(0).min(self.width as isize);
-        let y0 = y0.max(0).min(self.height as isize);
-        for (x, y) in line_drawing::Bresenham::new((x0, y0), (x1, y1)) {
-            if let Some(i) = self.grid_idx(x, y) {
-                self.particles[i].set_active(active);
-                self.particles[i].already_updated = false;
-                self.particles[i].p_type = self.active_type;
-            } else {
-                break;
-            }
-        }
-    }
-
-    fn getXYfromInx(&self, idx: usize) -> (usize, usize) {
-        let row: usize = idx % self.width;
-        let column: usize = idx / self.width;
-        (row, column)
-    }
-
-
-
-        /* Given an index in the array of X this function will return the index on the 8
-        neighbors in an array of len 8 where array[0] is the cell to the immediate right of ?
-        and the continue in a clowise fashion. If the cell is touching an edge of the game board the value 
-        for neighbors that are off the board is -1
-
-        [5]  [6]  [7]
-
-        [4]   X   [0]
-
-        [3]  [2]  [1]
-
-        Example 1) Cell X is in the middle of a 3x3 game board
-
-        idx = 4 and the board would look like this
-        0  1  2
-        3  4  5
-        6  7  8
-        The returned vector is <5,8,7,6,3,0,1,2>
-
-        Example 2) Cell X is in the upper left corner of a 3x3 board
-        idx = 0 and the board would look like this, remember -1 indicataes a wall or edge
-        -1 -1 -1
-        -1  0  1
-        -1  3  4
-        The returned vector is <1,4,3,-1,-1,-1,-1,-1>
-        */
-    fn getEightNeighbors(&self, idx: usize) -> Vec<isize> {
-        let coord = self.getXYfromInx(idx);
-        let mut v: Vec<isize> = vec![-1; 8];
-        log::info!("[getEightNeighbors] Called with indx:{}, which maps to x,y:{:?}",idx, coord);
-        //println!("{}{:?}",idx, coord );
-
-        v[0] = if coord.0 == self.width-1 { -1 } else { (idx+1) as isize };
-        v[1] = if coord.0 == self.width-1 || coord.1 == self.height -1 { -1 } else {(idx + 1 + self.width) as isize};
-        v[2] = if coord.1 == self.height -1 { -1 } else { (idx+ self.width) as isize };
-        v[3] = if coord.0 == 0 || coord.1 == self.height -1 { -1 } else { (idx+ self.width -1 ) as isize };
-        v[4] = if coord.0 == 0 {-1} else {(idx-1) as isize};
-        v[5] = if coord.1 == 0 || coord.0 == 0 {-1} else {(idx - 1 - self.width) as isize};
-        v[6] = if coord.1 == 0 {-1} else {(idx - self.width) as isize};
-        v[7] = if coord.0 == self.width-1 || coord.1 == 0 {-1} else {(idx + 1 - self.width) as isize};
-        log::debug!("[getEightNeighbors] {:?}",v);
-        v
-    }
-
-    fn printCrazy8(&self, x: Vec<isize>, idx: usize){
-
-        println!();
-        println!("{} {} {}",x[5], x[6], x[7] );
-        println!("{} {} {}",x[4], idx, x[0]);
-        println!("{} {} {}",x[3],x[2], x[1]);
-        println!();
-    }
-
-    fn grid_idx<I: std::convert::TryInto<usize>>(&self, x: I, y: I) -> Option<usize> {
-        if let (Ok(x), Ok(y)) = (x.try_into(), y.try_into()) {
-            if x < self.width && y < self.height {
-                Some(x + y * self.width)
-            } else {
-                None
-            }
-        } else {
-            None
-        }
-    }
-}
-
-/*
-fn main() {
-
-    let a:i32 = 0;
-    let b:i32 = 100;
-    let width:i32 = 10;
-    let mut c:i8  = 0;
-    for x in a..b{ 
-        if x<width{
-             c = (x.overflowing_sub(width)).0 as i8;
-        }
-        else{
-            c = (x-width) as i8;
-            }
-        println!("{}", c);
-    }
-    }
-
-    */
